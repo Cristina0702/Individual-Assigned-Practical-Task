359e22a757f9fba0ee701eb119d4bced989bba9c
Cristina Cutajar <cristinacutajar@gmail.com>
2022-04-20 15:25:10 +0200

    Code Upload

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
-import java.util.ArrayList;
 public class Main{
 
-
-
     public static void main(String[] args) {
-        
+
+        Risk r = new Risk();
     }
 }
--- Risk.java
+++ Risk.java
@@ -1,49 +1,70 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Observable;
+import java.util.Random;
+
+import java.lang.StringBuilder;
+import java.lang.IllegalArgumentException;
+import java.lang.NumberFormatException;
+
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+import javax.swing.JFrame;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 public class Risk {
     private String[] countriesArray;
-    private String[] continentsArray;
-
+    private String[] adjacenciesArray;
+    private String line;
+    private String input;
     
-	private ArrayList<Country> countries;
-	private ArrayList<Country> priorityCountries;
-	private ArrayList<Country> priorityTargets;
+    private ArrayList<Country> countries;
+    private ArrayList<Country> priorityCountries;
+    private ArrayList<Country> priorityTargets;
 
+      
+    private Country countryA;
+    private Country countryB;
+
+    private StringBuilder stringBuilder;
+	  private BufferedReader reader;
+
+    protected Risk() {
+	
+    }
     
-	private Country countryA;
-	private Country countryB;
-
-    countriesArray = {"Afghanistan", "Alaska", "Alberta", "Argentina", "Brazil", "Central America", "China",
-    "Congo", "East Africa", "Eastern Australia", "Eastern United States", "Egypt", "Great Britain", 
-    "Greenland", "Iceland",  "India", "Indonesia", "Irkutsk", 
+    protected void saveGame() {
     
-    ""Northwest Territory"", ""Ontario"", ""Quebec"",
-    ""Western United States"", ""Venezuela""
-        
-        
-        "Northern Europe"
-        "Scandinavia"
-        "Southern Europe"
-        "Ukraine"
-        "Western Europe"
-        
-        "Peru"
-        
-        "Madagascar"
-        "North Africa"
-      "  South Africa"
-        
-        
-       
-        
-        "Japan"
-        "Kamchatka"
-        "Middle East"
-        "Mongolia"
-        "Siam"
-        "Siberia"
-        "Ural"
-        ""Yakutsk"", ""New Guinea"" ,""Western Australia""}
+    }
 
-        central america --> mexico
+    protected void quitGame() {
+      System.exit(0);
+    }
+
+    public void hi()throws FileNotFoundException{
+    
+      try{
+        // Reads adjacencies file
+        reader = new BufferedReader(new FileReader("adjacencent_countries.txt"));			
+        stringBuilder = new StringBuilder();
+        
+        while((line = reader.readLine()) != null) {
+          stringBuilder.append(line);
+        }
+        input = stringBuilder.toString();
+        
+        // Creates an array of each line from the file
+        adjacenciesArray = input.split("\t");
+      } catch (FileNotFoundException error) {
+        System.out.println(error.getMessage());
+      } catch (IOException error) {
+        System.out.println(error.getMessage());
+      }
+  }
+  
 }
